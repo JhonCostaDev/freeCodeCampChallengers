708ced49227123bb86fc9db024838f01213bba9b
Jonathan Costa <jhoncosta2@gmail.com>
2026-03-07 11:56:40 -0300

    create rpg_character lab folder and add init files

---
+++ python_certification/rpg_character/test.py
@@ -0,0 +1,13 @@
+name = 'adamastor pitaco'
+#print(isinstance(name, str))
+
+#if not name:
+    #print(name)
+# if len(name) > 10:
+#     print('The character name is too long')
+# print(len(name))
+
+if ' ' in name:
+    print('The character name should not contain spaces.')
+else:
+    print(name)
