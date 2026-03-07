c963053251141cfe3e3e1306812e8b7d71ee7889
Jonathan Costa <jhoncosta2@gmail.com>
2026-03-07 12:09:56 -0300

    validate stats

--- python_certification/rpg_character/rpg_character.py
+++ python_certification/rpg_character/rpg_character.py
@@ -1,4 +1,4 @@
-def create_character(name, strength, intelligence, charisma):
+def create_character(name:str, strength:int, intelligence:int, charisma:int):
     if not isinstance(name, str):
         return 'The character name should be a string.'
     elif not name:
@@ -7,3 +7,12 @@ def create_character(name, strength, intelligence, charisma):
         return 'The character name is too long.'
     elif ' ' in name:
         return 'The character name should not contain spaces.'
+    
+    if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+        return 'return All stats should be integers.'
+    elif strength < 1 or intelligence < 1 or charisma < 1:
+        return ' All stats should be no less than 1'
+    elif strength > 4 or intelligence > 4 or charisma > 4:
+        return 'All stats should be no more than 4.'
+    elif sum([strength, intelligence, charisma]) > 7:
+        return 'The character should start with 7 points.'
--- python_certification/rpg_character/test.py
+++ python_certification/rpg_character/test.py
@@ -1,13 +1,26 @@
 name = 'adamastor pitaco'
-#print(isinstance(name, str))
+strength = 1
+intelligence = 1
+charisma = 3
 
+
+#print(isinstance(name, str))
+# if not isinstance(stats, int):
+#     print('not a number')
 #if not name:
     #print(name)
 # if len(name) > 10:
 #     print('The character name is too long')
 # print(len(name))
 
-if ' ' in name:
-    print('The character name should not contain spaces.')
-else:
-    print(name)
+# if ' ' in name:
+#     print('The character name should not contain spaces.')
+# else:
+#     print(name)
+
+# if strength < 1 or intelligence < 1 or charisma < 1:
+#     print('Negativo')
+# else:
+#     print('ok')
+
+print(sum((strength, intelligence, charisma)))
